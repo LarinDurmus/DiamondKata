380fa56ba78ebd7d30dfdf8ed5042272f81abc99
LarinDurmus <62653723+LarinDurmus@users.noreply.github.com>
2020-05-22 00:29:18 +0200

    Update diamond.js

--- diamond.js
+++ diamond.js
@@ -4,7 +4,7 @@
     var diamond = new Array(26); 
  
     function giveDiamond(user_letter) {
-        // search for letter number in the array letter
+        // suche nach Buchstabe
         for ( i = 0; i < letter.length; i++) {
             if (letter[i] == user_letter) {
                 letter_number = i;
@@ -18,23 +18,22 @@
             for ( j = 0; j < letter_number - i; j++) {
                 diamond[i] += "-";
             }
- 
-            // add letter
+
             diamond[i] += letter[i];
  
-            // add space between letters
+            // abstände
             if (letter[i] != 'A') {
                 for ( j = 0; j < 2 * i - 1; j++) 
                 { 
                 diamond[i] += "-"; }
-                // add letter
+                
                 diamond[i] += letter[i];
                 } 
-                //add space after letters
+                //abstände nach buchstaben
                 for ( j = 0; j < letter_number - i; j++) {
                     diamond[i] += "-";
                 }
-                // Draw the first part of the diamond 
+       
                 console.log(diamond[i]);
                 } 
                 for ( i = letter_number - 1; i >= 0; i--)
